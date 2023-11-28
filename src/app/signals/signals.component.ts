b873e88716313c0e942f71bb50f44b41ac2d358c
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-28 13:25:45 -0500

    Created the signals component, added some html to the template that shows a counter with a plus and a minus sign. Added some css for the template.

---
+++ src/app/signals/signals.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  templateUrl: './signals.component.html',
+  styleUrls: ['./signals.component.css']
+})
+export class SignalsComponent {
+  counter: number = 0;
+  message: string[] = [];
+
+  increment(){
+
+  }
+
+  decrement(){
+    
+  }
+}
