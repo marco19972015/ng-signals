fce25a8ede6706fbde11e8312a3632a9260d6000
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-29 11:20:28 -0500

    Added the DoCheck life cycle hook to show an example of how the change dection cycle is called everytime we interact with the click event

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+
 
 @Component({
   selector: 'app-signals',
   templateUrl: './signals.component.html',
   styleUrls: ['./signals.component.css']
 })
-export class SignalsComponent {
+export class SignalsComponent implements DoCheck {
   counter: number = 0;
   message: string[] = [];
 
   increment(){
-
+    this.counter++;
   }
 
   decrement(){
+    this.counter--;
+  }
+
+  ngDoCheck(){
+    console.log('Angular change detection called');
     
   }
 }
