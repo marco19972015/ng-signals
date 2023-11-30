c6b8fc9004b6ca04d85c5ddbf64ad38c848c3706
Marco Torres <marcoantonio.torres@icloud.com>
2023-11-29 20:05:11 -0500

    Created two examples of working with update and mutate method. Showed the disadvantages of using update on values that are mutable.

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -6,32 +6,36 @@ import { Component, DoCheck, signal } from '@angular/core';
   styleUrls: ['./signals.component.css']
 })
 export class SignalsComponent implements DoCheck {
-  // Using the set() method to increment the value of the signal
-  // Using the update() method to increment the value of the signal
 
-
-  // Create a signal and assign it to counter prop
   counter = signal(0);
-  message: string[] = [];
+
+  // Create a signal and assign it to the message prop 
+  // we can specify the type since it is generic type at first
+  message = signal<string[]>([]);
 
   // Method to increment counter
   increment(){
-    // set method is being used to update the value of the counter signal based on the previous value
-    // set method should be used whenenever we want to set a new value and that new value does not depend on the previous value
-    // this.counter.set(this.counter() + 1);
-
-    // We should be using update method instead of set method because the update method receives the previous signal value so it depends on that prev value
-    // to the update method we need to pass a callback function
-    // the call back function receives the previous value of the counter, so angular sets that value as an argument
     this.counter.update((prevValue) => prevValue + 1);
+
+    // Problem with the code below is that it's creating a new array everytime we increment the size
+
+    // ...prevMessage extracts all the elements from the prevMessage array
+    // and those element then become an element of the new array
+    // then in that new array we can add that new value we want to push
+    this.message.update((prevMessage) => [...prevMessage, 'Current value of counter is: ' + this.counter()]);
+
+    // With this code we are not creating a new array, instead we are pushing the new element to the same existing array
+    this.message.mutate((prevMessage) => prevMessage.push('Current value of counter is: ' + this.counter()));
   }
 
   decrement(){
-    this.counter;
+    this.counter.update((prevValue) => prevValue - 1);
+
+    // remove the last element when the decrement button is clicked
+    this.message.mutate((prevMessage) => prevMessage.pop());
   }
 
   ngDoCheck(){
     console.log('Angular change detection called');
-    
   }
 }
